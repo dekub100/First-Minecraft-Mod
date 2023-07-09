a7d196c88b36566bc9d09cd59e1621dcfddf1f18
dekub100 <kuba911007@gmail.com>
2023-07-09 22:05:44 +0200

    Added: Eight Ball

--- src/main/java/net/dekub/testmod/item/ModItems.java
+++ src/main/java/net/dekub/testmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.dekub.testmod.item;
 
 import net.dekub.testmod.TestMod;
+import net.dekub.testmod.item.custom.EightBallItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
@@ -11,6 +12,8 @@ public class ModItems {
             new Item(new FabricItemSettings().group(ModItemGroup.RUBY)));
     public static final Item RUBY = registerItem("ruby",
             new Item(new FabricItemSettings().group(ModItemGroup.RUBY)));
+    public static final Item EIGHT_BALL = registerItem("eight_ball",
+            new EightBallItem(new FabricItemSettings().group(ModItemGroup.RUBY).maxCount(1)));
 
 
     private static Item registerItem(String name, Item item) {
