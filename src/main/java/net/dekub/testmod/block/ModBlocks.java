d961fd185f880f336b1cfc8fb5e1b8bf15556f6d
dekub100 <kuba911007@gmail.com>
2023-07-15 19:01:26 +0200

    Added Compressed Diamond Block

--- src/main/java/net/dekub/testmod/block/ModBlocks.java
+++ src/main/java/net/dekub/testmod/block/ModBlocks.java
@@ -18,6 +18,8 @@ import net.minecraft.util.registry.Registry;
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBlock("ruby_block", new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.RUBY);
 
+    public static final Block COMPRESSED_DIAMOND_BLOCK = registerBlock("compressed_diamond_block", new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.RUBY);
+
     public static final Block RUBY_ORE = registerBlock("ruby_ore", new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool(), UniformIntProvider.create(3, 7)), ModItemGroup.RUBY);
 
     public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore", new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool(), UniformIntProvider.create(3, 7)), ModItemGroup.RUBY);
